cbdeeb5b4be764d9fe46b01dcfa180e2656a310d
Ricky Chiang <metavige@gmail.com>
2020-06-16 20:49:44 +0800

    feat: add source

---
+++ mvn-base/src/main/java/com/example/demo/DampApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DampApplication {
+
+  // region Methods
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(DampApplication.class, args);
+  }
+
+  // endregion
+}
